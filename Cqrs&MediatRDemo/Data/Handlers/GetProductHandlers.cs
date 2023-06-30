42ec25bcf2a8de40a7768a8a4b08c50b975b4fb5
SOUMYA\ersau <ersaumyarout@gmail.com>
2023-06-30 13:36:51 +0530

    added get product by id handler

---
+++ Cqrs&MediatRDemo/Data/Handlers/GetProductHandlers.cs
@@ -0,0 +1,17 @@
+﻿using Cqrs_MediatRDemo.Data.Queries;
+using Cqrs_MediatRDemo.Models;
+using Cqrs_MediatRDemo.Services;
+using MediatR;
+
+namespace Cqrs_MediatRDemo.Data.Handlers
+{
+    public class GetProductHandlers : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductHandlers(IProductRepository productRepository) => _productRepository = productRepository;
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductListByIdAsync(request.Id);
+        }
+    }
+}
